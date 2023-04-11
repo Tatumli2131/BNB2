f66855a6b217c1a6780ab8770b5e9a967f02402a
VitaRon <119419667+Tatumli2131@users.noreply.github.com>
2023-04-11 10:57:01 +0800

    合约

---
+++ contracts/StandardImpl2.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.10;
+
+contract StandardImpl2 {
+    uint256 public constant VERSION = 2;
+
+    bool public initialized;
+
+    uint256 public value;
+
+    modifier initializer() {
+        require(!initialized, "Only initialize once");
+        _;
+        initialized = true;
+    }
+
+    function initialize(uint256 _initValue) public initializer {
+        value = _initValue;
+    }
+
+    function setValue(uint256 _newValue) public {
+        value = _newValue + 10;
+    }
+
+    function subValue(uint256 _newValue) public {
+        value = _newValue - 10;
+    }
+}
